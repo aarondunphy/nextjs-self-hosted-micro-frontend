b2a08d816d102ecb5a4102dcdfbfc9439da43d35
Aaron Dunphy <aaronjohndunphy@gmail.com>
2023-11-17 13:01:32 +0000

    Link to change micro frontends

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-  return <h1>Home is where VS Code is</h1>;
+  return (
+    <>
+      <h1>Home is where VS Code is</h1>
+      {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+      <a href="/">Change Micro Frontend</a>
+    </>
+  );
 }
